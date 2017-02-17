f324c20ffb5f32aa61956fb0cee0e32737c35d78
Kirill Babkin <babkin.kir@gmail.com>
2017-02-17 18:32:01 -0500

    removed depreciated methods.

--- script1.py
+++ script1.py
@@ -3,10 +3,13 @@ import pandas
 
 df = pandas.read_csv('data.txt')
 
+
 def color(elev):
-    if elev in range(0, 1000):
+    minimum = int(min(df['ELEV']))
+    step = int(max(df['ELEV'] - min(df['ELEV']))/3)
+    if elev in range(minimum, minimum+step):
         col = "green"
-    elif elev in range(1000, 3000):
+    elif elev in range( minimum+step,  minimum+step*2):
         col = "orange"
     else:
         col = "red"
@@ -17,14 +20,12 @@ map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()],
                  tiles='Stamen Terrain')
 
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    map.simple_marker(location=[lat, lon],
-                      popup=name,
-                      marker_color=color(elev)
-                      )
+    map.add_child(folium.Marker(
+        location=[lat, lon],
+        popup=name,
+        icon=folium.Icon(color=color(elev)))
+    )
 
-
-
-
-map.create_map(path="test.html")
+map.save(outfile="test.html")
 
 
